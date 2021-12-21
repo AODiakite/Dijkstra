a36a147ceacc3a55a28a26f5dfc3ea3f1269c52d
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2021-12-21 23:52:14 +0100

    Initial Commit

---
+++ Dijkstra/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Dijkstra
+//
+//  Created by Abdoul Oudouss Diakite on 21/12/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
